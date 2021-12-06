888c233c3f2499e8aa3871647afdcbb6bfa1cec2
Joe Ivans <contact@joeivans.dev>
2021-12-05 19:23:06 -0800

    Add image alt text - Added image alt text for screen readers

--- src/index.js
+++ src/index.js
@@ -8,53 +8,60 @@ function BookList() {
         title="The Big Book of Silly Jokes for Kids"
         author="Carole P. Roman"
         imgSrc="https://images-na.ssl-images-amazon.com/images/I/81QOQobJiGL._AC_UL200_SR200,200_.jpg"
+        imgAlt="The Big Book of Silly Jokes for Kids"
+      />
+      <Book
+        title="If Animals Kissed Good Night"
+        author="Ann Whitford Paul"
+        imgSrc="https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg"
+        imgAlt="If Animals Kissed Good Night"
       />
       <Book
         title="The Big Book of Silly Jokes for Kids"
         author="Carole P. Roman"
         imgSrc="https://images-na.ssl-images-amazon.com/images/I/81QOQobJiGL._AC_UL200_SR200,200_.jpg"
+        imgAlt="The Big Book of Silly Jokes for Kids"
       />
       <Book
         title="The Big Book of Silly Jokes for Kids"
         author="Carole P. Roman"
         imgSrc="https://images-na.ssl-images-amazon.com/images/I/81QOQobJiGL._AC_UL200_SR200,200_.jpg"
+        imgAlt="The Big Book of Silly Jokes for Kids"
       />
       <Book
         title="The Big Book of Silly Jokes for Kids"
         author="Carole P. Roman"
         imgSrc="https://images-na.ssl-images-amazon.com/images/I/81QOQobJiGL._AC_UL200_SR200,200_.jpg"
+        imgAlt="The Big Book of Silly Jokes for Kids"
       />
       <Book
         title="The Big Book of Silly Jokes for Kids"
         author="Carole P. Roman"
         imgSrc="https://images-na.ssl-images-amazon.com/images/I/81QOQobJiGL._AC_UL200_SR200,200_.jpg"
+        imgAlt="The Big Book of Silly Jokes for Kids"
       />
       <Book
         title="The Big Book of Silly Jokes for Kids"
         author="Carole P. Roman"
         imgSrc="https://images-na.ssl-images-amazon.com/images/I/81QOQobJiGL._AC_UL200_SR200,200_.jpg"
-      />
-      <Book
-        title="The Big Book of Silly Jokes for Kids"
-        author="Carole P. Roman"
-        imgSrc="https://images-na.ssl-images-amazon.com/images/I/81QOQobJiGL._AC_UL200_SR200,200_.jpg"
+        imgAlt="The Big Book of Silly Jokes for Kids"
       />
     </section>
   );
 }
 
-const Book = ({ title, author, imgSrc }) => {
+const Book = ({ title, author, imgSrc, imgAlt }) => {
   return (
-    <article>
+    <article className="book">
+      <Image src={imgSrc} alt={imgAlt} />
       <Title title={title} />
       <Author author={author} />
-      <Image src={imgSrc} />
     </article>
   );
 };
 
 const Title = ({ title }) => <h1>{title}</h1>;
 const Author = ({ author }) => <p>{author}</p>;
-const Image = ({ src }) => <img src={src} alt="" />;
+const Image = ({ src, alt }) => <img src={src} alt={alt} />;
 
 ReactDOM.render(<BookList />, document.getElementById('root'));
